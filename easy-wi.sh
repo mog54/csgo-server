5d150596e2051dfb89b9f347aced7042f3d4b82b
mog54 <petitdalouzy@hotmail.fr>
2017-12-03 21:12:49 +0100

    Add files via upload

---
+++ easy-wi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get install libxml-parser-perl libpath-class-perl perl-modules screen rsync sudo e2fsprogs unzip subversion pure-ftpd libarchive-zip-perl libc6 libgcc1 git curl -y
+apt-get install libc6-i386 lib32gcc1 -y
+apt-get install libhttp-daemon-perl -y
+apt-get install libarchive-extract-perl -y
+adduser ogpbot
+sudo usermod -a -G sudo ogpbot
+
+sudo -u ogpbot bash << EOF
+
+wget https://github.com/OpenGamePanel/OGP-Agent-Linux/archive/master.zip
+unzip master.zip
+cd OGP-Agent-Linux-master/
+sudo bash ./install.sh
